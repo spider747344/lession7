b62195ff6e782369622e7a922c4c189fcef6e838
spider747344 <9630003948sri@gmail.com>
2025-04-01 20:52:47 +0530

    c:\Users\SRIJAN SARKAR\OneDrive\Desktop\Python classes\lession7\activity2.py

--- activity2.py
+++ activity2.py
@@ -1,24 +1,19 @@
 c = int(input("enter your electricity consumtion"))
-if  ( c < 50 ):
+if   c < 50 :
   b = ( c * 2.60)+25
   print = ( "your electricity bill is",b)
 
-elif (c > 50) and (c < 100): 
+elif c >= 50 and c < 100: 
   b = ( c * 3.25)+35
   print = ("your electricity bill is",b)
 
-if (c < 100):
-  b = (c * 5.26)+45
-  print = ("your electricity bill is",b)
 
-elif (c > 100) and (c < 200):
+
+elif c >= 100 and c < 200:
   b = (c * 5.26)+45
   print= ("your electricity bill is",b)
 
-  if (c > 200 ):
-   b = (c * 8.45)+75
-  print = ("your electricity bill is",b)
 
-elif (c > 200) and (c < 300):
+elif c > 200 :
   b =  (c * 8.45)+75
-print = ("your electricity bill is",b)
+  print = ("your electricity bill is",b)
